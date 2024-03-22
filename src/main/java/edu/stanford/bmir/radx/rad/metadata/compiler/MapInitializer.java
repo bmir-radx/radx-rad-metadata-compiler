42c447d7a3366a6fb2f061c14ebbdbc4b7d83d7d
Yan Cao <ycao77@stanford.edu>
2024-03-22 10:20:24 -0700

    Change URI to IRI

--- src/main/java/edu/stanford/bmir/radx/rad/metadata/compiler/MapInitializer.java
+++ src/main/java/edu/stanford/bmir/radx/rad/metadata/compiler/MapInitializer.java
@@ -4,33 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MapInitializer {
-  private static final String personUri = "http://vocab.fairdatacollective.org/gdmt/Person";
-  private static final String orcidUri = "https://orcid.org/";
-  private static final String rorUri = "https://ror.org/";
-  private static final String projectLeaderUri = "http://vocab.fairdatacollective.org/gdmt/ProjectLeader";
-  private static final String createdUri = "http://vocab.fairdatacollective.org/gdmt/Created";
-  private static final String urlUri = "http://vocab.fairdatacollective.org/gdmt/URL";
-  private final static String meshUri =  "http://purl.bioontology.org/ontology/MESH";
+  private static final String personIri = "http://vocab.fairdatacollective.org/gdmt/Person";
+  private static final String orcidIri = "https://orcid.org/";
+  private static final String rorIri = "https://ror.org/";
+  private static final String projectLeaderIri = "http://vocab.fairdatacollective.org/gdmt/ProjectLeader";
+  private static final String createdIri = "http://vocab.fairdatacollective.org/gdmt/Created";
+  private static final String urlIri = "http://vocab.fairdatacollective.org/gdmt/URL";
+  private final static String meshIri =  "http://purl.bioontology.org/ontology/MESH";
   //TODO: need to update
-  private final static String ueiUri = "https://uei.org/";
-  private final static String piUri = "https://w3id.org/gdmt/PI";
-  private final static String contactPiUri = "https://w3id.org/gdmt/ContactPI";
-  private final static String dataPiUri = "https://w3id.org/gdmt/DataPI";
+  private final static String ueiIri = "https://uei.org/";
+  private final static String piIri = "https://w3id.org/gdmt/PI";
+  private final static String contactPiIri = "https://w3id.org/gdmt/ContactPI";
+  private final static String dataPiIri = "https://w3id.org/gdmt/DataPI";
 
 
   public static Map<String, String> createControlledTermsMap(){
     Map<String, String> controlledTermsMap = new HashMap<>();
-    controlledTermsMap.put("Person", personUri);
-    controlledTermsMap.put("ORCiD", orcidUri);
-    controlledTermsMap.put("ROR", rorUri);
-    controlledTermsMap.put("UEI", ueiUri);
-    controlledTermsMap.put("Project Leader", projectLeaderUri);
-    controlledTermsMap.put("Created", createdUri);
-    controlledTermsMap.put("URL", urlUri);
-    controlledTermsMap.put("MESH", meshUri);
-    controlledTermsMap.put("contact-PI", contactPiUri);
-    controlledTermsMap.put("data-PI", dataPiUri);
-    controlledTermsMap.put("PI", piUri);
+    controlledTermsMap.put("Person", personIri);
+    controlledTermsMap.put("ORCiD", orcidIri);
+    controlledTermsMap.put("ROR", rorIri);
+    controlledTermsMap.put("UEI", ueiIri);
+    controlledTermsMap.put("Project Leader", projectLeaderIri);
+    controlledTermsMap.put("Created", createdIri);
+    controlledTermsMap.put("URL", urlIri);
+    controlledTermsMap.put("MESH", meshIri);
+    controlledTermsMap.put("contact-PI", contactPiIri);
+    controlledTermsMap.put("data-PI", dataPiIri);
+    controlledTermsMap.put("PI", piIri);
     return controlledTermsMap;
   }
 }
